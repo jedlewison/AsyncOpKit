68716b3fd62cc27e02bfa605f5294260313f4774
Jed Lewison <jed@magicappfactory.com>
2015-05-24 08:48:20 -0700

    Updated test -- still WIP, however

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -4,122 +4,156 @@ import Nimble
 import AsyncOpKit
 
 class AsyncOpKitTests: QuickSpec {
-
+    
     override func spec() {
-
-        describe("Starting an AsyncOperation") {
-
-            var subject : JDAsyncOperation! = nil
-            var opQ : NSOperationQueue! = nil
-            var resultsObject : JDAsyncOperationResults!
-
-
+        describe("The behavior of an AsyncOperation") {
+            
+            var subject : JDAsyncOperation? = nil
+            var resultsObject : JDAsyncOperationResults? = nil
+            var resultsHandlerCompleted : Bool? = nil
+            
             beforeEach {
+                resultsObject = nil
+                resultsHandlerCompleted = false
+                
                 subject = JDAsyncOperation()
-                subject.completionHandler = {
+                subject?.completionHandler = {
                     result in
                     resultsObject = result
+                    resultsHandlerCompleted = true
                 }
-                opQ = NSOperationQueue()
             }
-
+            
+            afterEach {
+                resultsObject = nil
+                resultsHandlerCompleted = nil
+            }
+            
             context("when it starts normally") {
-
+                
                 beforeEach {
-                    opQ.addOperation(subject)
-                }
-
-                it("should turn to the executing state immediately after being started") {
-                    expect(subject.executing).to(beTrue())
-                }
-
-                it("should not have completed yet") {
-                    expect(resultsObject).to(beNil())
-                    expect(resultsObject.operation).toEventually(equal(subject))
-
-                }
-
-            }
-
-            context("when it is canceled before starting") {
-
-                beforeEach {
-                    subject.cancel()
-                    //                    subject.start()
-                    opQ.addOperation(subject)
-                }
-
-                it("should be cancelled") {
-                    expect(subject.cancelled).to(beTrue())
-                }
-
-                it("should be finished") {
-                    expect(subject.finished).toEventually(beTrue())
-                }
-
-                it("should stop executing") {
-                    expect(subject.executing).to(beFalse())
-                }
-
-            }
-
-            context("when an operation finishes normally") {
-
-                beforeEach {
-                    //                    subject.start()
-                    opQ.addOperation(subject)
-                    //                    subject.start()
-                    //                    subject.finish()
-                }
-
-                it("should not be cancelled") {
-                    expect(subject.cancelled).to(beFalse())
-                }
-
-                it("should eventually be finished") {
-                    expect(subject.finished).to(beFalse())
-                    expect(subject.finished).toEventually(beTrue())
-                }
-
-                it("should start executing and stop executing") {
-                    expect(subject.executing).toEventually(beTrue())
-                    expect(subject.executing).toEventually(beFalse())
-                }
-
-            }
-
-            context("when an operation is started after being finished") {
-
-                beforeEach {
-                    opQ.addOperations([subject], waitUntilFinished: true)
-                    subject.start()
-                }
-
-                it("should still be finished") {
-                    expect(subject.finished).to(beTrue())
-                }
-
-                it("should not be executing") {
-                    expect(subject.executing).to(beFalse())
-                }
-
-            }
-
-            context("When an operation is cancelled after being started but before completion") {
-
-                beforeEach {
-                    //                    subject.start()
-                    opQ.addOperation(subject)
-                    subject.cancel()
-                }
-
-                it("should immediatelhy be marked as cancelled") {
-                    expect(subject.cancelled).to(beTrue())
+                    subject?.start()
                 }
                 
-                it("should not still be executing if handleCancellation synchronously finishes the op") {
-                    expect(subject.executing).to(beFalse())
-                    expect(subject.finished).to(beTrue())
+                describe("immediately after starting") {
+                    it("should be executing") {
+                        expect(subject?.executing).to(beTrue())
+                    }
+                    
+                    it("should not be finished") {
+                        expect(subject?.finished).to(beFalse())
+                    }
+                    
+                    it("should not execute its results handler") {
+                        expect(resultsHandlerCompleted).to(beFalse())
+                    }
+                    
+                    it("should still have a results handler") {
+                        expect(subject?.completionHandler).toNot(beNil())
+                    }
+                }
+                
+                describe("asynchronously after starting") {
+                    it("should stop executing") {
+                        expect(subject?.executing).toEventually(beFalse())
+                    }
+                    
+                    it("should finish") {
+                        expect(subject?.finished).toEventually(beTrue())
+                    }
+                    
+                    it("should execute its results handler") {
+                        expect(resultsHandlerCompleted).toEventually(beTrue())
+                    }
+                    
+                    it("should nil out the results handler") {
+                        expect(subject?.completionHandler).toEventually(beNil())
+                    }
+                    
+                    it("should return itself in its results handler resultsObject") {
+                        expect(resultsObject?.operation).toEventually(equal(subject))
+                    }
+                }
+                
+                context("when an operation finishes normally") {
+                    
+                    beforeEach {
+                        subject?.finish()
+                    }
+                    
+                    it("should not be cancelled") {
+                        expect(subject?.cancelled).toEventuallyNot(beTrue())
+                    }
+                    
+                    it("should eventually be finished") {
+                        expect(subject?.finished).toEventually(beTrue())
+                    }
+                    
+                    it("should start executing and stop executing") {
+                        expect(subject?.executing).toEventually(beFalse())
+                    }
+                    
+                    context("when an operation is started after being finished") {
+                        
+                        beforeEach {
+                            subject?.start()
+                        }
+                        
+                        it("should still be finished") {
+                            expect(subject?.finished).to(beTrue())
+                        }
+                        
+                        it("should not be executing") {
+                            expect(subject?.executing).to(beFalse())
+                        }
+                        
+                    }
+
+                }
+                
+                context("When an operation is cancelled after being started but before completion") {
+                    
+                    beforeEach {
+                        subject?.cancel()
+                    }
+                    
+                    it("should immediatelhy be marked as cancelled") {
+                        expect(subject?.cancelled).to(beTrue())
+                    }
+                    
+                    it("should not still be executing if handleCancellation synchronously finishes the op") {
+                        expect(subject?.executing).to(beFalse())
+                        expect(subject?.finished).to(beTrue())
+                    }
+                }
+
+            }
+            
+            context("when it is canceled before starting") {
+                
+                beforeEach {
+                    subject?.cancel()
+                    subject?.start()
+                }
+                
+                it("should immediately cancel") {
+                    expect(subject?.cancelled).to(beTrue())
+                }
+                
+                it("should be finished") {
+                    expect(subject?.finished).to(beTrue())
+                }
+                
+                it("should not start executing") {
+                    expect(subject?.executing).to(beFalse())
+                }
+                
+                it("should never be executing") {
+                    expect(subject?.executing).toEventuallyNot(beTrue())
+                }
+                
+                it("the resultsHandler's resultsObject should mark it as cancelled") {
+                    expect(resultsObject?.cancelled).toEventually(beTrue())
                 }
             }
             
