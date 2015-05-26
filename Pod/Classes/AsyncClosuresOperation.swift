84a8bc807985a7301c786f9ad5ae034411254489
Jed Lewison <jed@magicappfactory.com>
2015-05-25 20:36:01 -0700

    Put interface items on top; make closure queue a computed property that can only be set once. (Will make this a property based on an enum.)

--- Pod/Classes/AsyncClosuresOperation.swift
+++ Pod/Classes/AsyncClosuresOperation.swift
@@ -3,10 +3,9 @@ public class AsyncClosuresOperation : AsyncOperation {
     public typealias AsyncClosureIdentifier = Int
     public typealias AsyncClosure = (op: AsyncClosuresOperation, closureIdentifier: AsyncClosureIdentifier) -> Void
     
-    private var closures = Dictionary<AsyncClosureIdentifier, AsyncClosure>()
-    private var currentClosureIdentifier = 0
-    private var numberOfClosures = 0
-    public var closureQueue = dispatch_get_main_queue() // must be a serial queue!
+    public func markClosureWithIdentifierFinished(closureIdentifier: AsyncClosureIdentifier) {
+        performClosureWithIdentifier(closureIdentifier + 1)
+    }
     
     public func addAsyncClosure(asyncClosure: AsyncClosure) {
         dispatch_async(closureQueue) {
@@ -15,16 +14,31 @@ public class AsyncClosuresOperation : AsyncOperation {
         }
     }
     
-    public func markClosureWithIdentifierFinished(closureIdentifier: AsyncClosureIdentifier) {
-        performClosureWithIdentifier(closureIdentifier + 1)
+    public var closureQueue: dispatch_queue_t {
+        get {
+            if let _ = _closureQueue {
+                return _closureQueue!
+            } else {
+                return dispatch_get_main_queue()
+            }
+        }
+        set {
+            if let _ = _closureQueue {
+                println("Cannot reassign closure queue")
+            } else {
+                _closureQueue = newValue
+            }
+        }
     }
+    
+    private var closures = Dictionary<AsyncClosureIdentifier, AsyncClosure>()
+    private var currentClosureIdentifier = 0
+    private var numberOfClosures = 0
+
+    private var _closureQueue: dispatch_queue_t? = nil
     
     override public func main() {
         performClosureWithIdentifier(0)
-    }
-    
-    override public func handleCancellation() {
-        // empty implementation
     }
     
     private func performClosureWithIdentifier(closureIdentifier: AsyncClosureIdentifier) {
