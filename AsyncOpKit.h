d440ee9e42d8fc0f4e2e3c9316ce7b18518230db
Jed Lewison <jed@magicappfactory.com>
2015-12-07 16:58:56 -0800

    Add a proper framework header to AsyncOpKit

---
+++ AsyncOpKit.h
@@ -0,0 +1,12 @@
+//
+//  AsyncOpKit.h
+//  AsyncOpKit
+//
+//  Created by Jed Lewison on 12/7/15.
+//  Copyright © 2015 Magic App Factory. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double AsyncOpKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char AsyncOpKitVersionString[];
