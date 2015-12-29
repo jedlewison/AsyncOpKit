899743cec48f49ccac392c43cce69362d91fdab7
Jed Lewison <jed@magicappfactory.com>
2015-12-28 20:32:31 -0800

    Added OS X target

---
+++ AsyncOpKit-OSX/AsyncOpKit-OSX.h
@@ -0,0 +1,19 @@
+//
+//  AsyncOpKit-OSX.h
+//  AsyncOpKit-OSX
+//
+//  Created by Jed Lewison on 12/28/15.
+//  Copyright © 2015 Magic App Factory. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for AsyncOpKit-OSX.
+FOUNDATION_EXPORT double AsyncOpKit_OSXVersionNumber;
+
+//! Project version string for AsyncOpKit-OSX.
+FOUNDATION_EXPORT const unsigned char AsyncOpKit_OSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AsyncOpKit_OSX/PublicHeader.h>
+
+
